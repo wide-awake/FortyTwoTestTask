6ce73d3bec6e05338709cf63b80c630b543d3592
Yanik Koval <yanikkoval@yandex.com>
2015-05-08 08:32:18 +0300

    ticket:3 active middlware & some fixes

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -52,6 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'activity.middlware.ReqCatchMiddleware',
 )
 
 ROOT_URLCONF = 'fortytwo_test_task.urls'
