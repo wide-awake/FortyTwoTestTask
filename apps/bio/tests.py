f50f123703dc310d044b6a0d4dde8233fc4661d4
yanik <yanikkoval@gmail.com>
2015-05-30 20:20:30 +0300

    ticket:5 - create test for feauture form

--- apps/bio/tests.py
+++ apps/bio/tests.py
@@ -36,11 +36,38 @@ class BioBaseTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class BioFormTestCase(TestCase):
+class PersonFormTestCase(TestCase):
 
     def setUp(self):
-        pass
+        Person.objects.all().delete()
+        self.p = PersonFactory()
+        self.p.save()
 
-    def test_math(self):
-        # check if math still works :)
-        self.assertEqual(2+2, 4)
+    def test_form(self):
+        update_url = reverse('bio:edit')
+        r = self.client.get(update_url)
+        data = r.context['form'].initial
+        # generate new data
+        new_data = PersonFactory()
+        # update some data
+        data['first_name'] = new_data.first_name
+        data['last_name'] = new_data.last_name
+        data['date_of_birth'] = new_data.date_of_birth
+        data['bio'] = new_data.bio
+        data['email'] = new_data.email
+        data['jabber'] = new_data.jabber
+        data['skype'] = new_data.skype
+        data['other'] = new_data.other
+        # post to the form
+        r = self.client.post(update_url, data)
+        # retrieve again
+        r = self.client.get(update_url)
+        # self.assertContains(r, 'updated_value')
+        self.assertEqual(r.context['form'].initial['first_name'], new_data.first_name)
+        self.assertEqual(r.context['form'].initial['last_name'], new_data.last_name)
+        self.assertEqual(r.context['form'].initial['date_of_birth'], new_data.date_of_birth)
+        self.assertEqual(r.context['form'].initial['bio'], new_data.bio)
+        self.assertEqual(r.context['form'].initial['email'], new_data.email)
+        self.assertEqual(r.context['form'].initial['jabber'], new_data.jabber)
+        self.assertEqual(r.context['form'].initial['skype'], new_data.skype)
+        self.assertEqual(r.context['form'].initial['other'], new_data.other)
