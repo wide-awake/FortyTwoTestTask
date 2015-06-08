9efe9b3f29205ec7b206162672fec36df2ec97a0
Yanik Koval <yanikkoval@yandex.com>
2015-06-08 16:20:14 +0300

    ticket:5 image scaling

--- apps/bio/models.py
+++ apps/bio/models.py
@@ -1,6 +1,11 @@
+import StringIO
+from PIL import Image as Img
+
 from django.db import models
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from utils.generate_upload_name import generate_upload_name
+
 
 class Person(models.Model):
     first_name = models.CharField(max_length=255)
@@ -12,6 +17,16 @@ class Person(models.Model):
     skype = models.CharField(max_length=255)
     other = models.TextField()
     photo = models.ImageField(upload_to='%Y/%m/%d', null=True)
+
+    def save(self, *args, **kwargs):
+        if self.photo:
+            image = Img.open(StringIO.StringIO(self.photo.read()))
+            image.thumbnail((200,200), Img.ANTIALIAS)
+            output = StringIO.StringIO()
+            image.save(output, format='JPEG', quality=75)
+            output.seek(0)
+            self.photo = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.photo.name, 'image/jpeg', output.len, None)
+        super(Person, self).save(*args, **kwargs)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
