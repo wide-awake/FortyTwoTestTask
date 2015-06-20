f90ab1a2fe49f6ca5eb3ae5e72a8acd5c4c94099
Yanik Koval <yanikkoval@gmail.com>
2015-06-20 11:05:55 +0300

    ticket:1 fix settings

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'fortytwo_test_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
