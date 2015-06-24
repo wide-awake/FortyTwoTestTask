60b7ce5b76ad2f9e43ea79a0b14599ee01838f40
Yanik Koval <yanikkoval@gmail.com>
2015-06-24 14:58:26 +0300

    ticket:1 remove admin media prefix

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -105,7 +105,6 @@ MEDIA_URL = '/uploads/'
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-ADMIN_MEDIA_PREFIX = os.path.join(STATIC_ROOT, 'admin')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
