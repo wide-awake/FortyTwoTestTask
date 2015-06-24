cc5761d83eb7dee7cac22075293b38b48b61c8cb
Yanik Koval <yanikkoval@gmail.com>
2015-06-24 15:10:18 +0300

    admin media

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, patterns, url
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,12 +10,11 @@ urlpatterns = patterns(
     url(r'^requests/', include('activity.urls', namespace='activity')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # for local development
 # if settings.DEBUG:
-urlpatterns += patterns(
-    'django.views.static',
-    url(r'^static/(.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),
-    url(r'^static/(.*)$', 'serve', {'document_root': settings.STATICFILES_DIRS[0]}),
-    url(r'^uploads/(.*)$', 'serve', {'document_root': settings.MEDIA_ROOT}))
+#     urlpatterns += patterns(
+#         'django.views.static',
+#         url(r'^static/(.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),
+#         url(r'^uploads/(.*)$', 'serve', {'document_root': settings.MEDIA_ROOT}))
