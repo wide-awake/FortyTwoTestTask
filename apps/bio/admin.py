8ff5541a885566023412e575dfbcd9185da556f8
Yanik Koval <yanikkoval@gmail.com>
2015-06-25 23:33:49 +0300

    ticket:1 admin formating#2

--- apps/bio/admin.py
+++ apps/bio/admin.py
@@ -4,8 +4,8 @@ from .models import Person, ChangeLog
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'email',
-                    'jabber', 'skype')
-    readonly_fields = ('admin_image',)
+                    'jabber', 'skype', 'admin_image')
+    # readonly_fields = ('admin_image',)
 
 
 class ChangeLogAdmin(admin.ModelAdmin):
