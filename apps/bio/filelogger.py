00e81bd820978f93f371565d4b2e1cbf63258e95
Yanik Koval <yanikkoval@gmail.com>
2015-07-13 18:02:27 +0300

    a bit of cleaning

--- apps/bio/filelogger.py
+++ apps/bio/filelogger.py
@@ -3,8 +3,6 @@ from __future__ import print_function
 import os
 import logging
 import logging.config
-from datetime import datetime
-import pytz
 
 from django.conf import settings
 
@@ -25,15 +23,3 @@ LOGGING = {
 
 logging.config.dictConfig(LOGGING)
 logger = logging.getLogger(__name__)
-
-
-model_log = \
-    lambda i: logger.debug([(k, v) for k, v in i.__dict__.iteritems()
-                            if not k.startswith("_")])
-
-req_log = \
-    lambda r: logger.info("[{}] path: {}, GET: {}, POST: {}".format(
-        datetime.now(pytz.timezone(settings.TIME_ZONE)),
-        r.path,
-        r.GET,
-        r.POST))
