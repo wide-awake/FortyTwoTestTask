620a086956fc27a769c3624554016d2430b64470
Yanik Koval <yanikkoval@gmail.com>
2015-07-13 18:00:25 +0300

    omg

--- apps/bio/views.py
+++ apps/bio/views.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+from datetime import datetime
+
 from django.shortcuts import render_to_response
 from django.views.generic import FormView
 from django.forms.models import model_to_dict
@@ -6,20 +8,28 @@ from django.http import HttpResponse
 
 from .models import Person
 from .forms import PersonForm
-from .filelogger import model_log, req_log
+from .filelogger import logger
 
 
 def single(request):
     p = Person.objects.all()[0]
-    model_log(p)
-    req_log(request)
+    logger.debug(p)
+    logger.info("[{}] path: {}, GET: {}, POST: {}".format(
+        datetime.now(),
+        request.path,
+        request.GET,
+        request.POST))
     return render_to_response('bio/person.html', {'object': p})
 
 
 def ajax_update(request):
     p = Person.objects.all()[0]
-    model_log(p)
-    req_log(request)
+    logger.debug(p)
+    logger.info("[{}] path: {}, GET: {}, POST: {}".format(
+        datetime.now(),
+        request.path,
+        request.GET,
+        request.POST))
     if request.POST:
         form = PersonForm(request.POST, request.FILES, instance=p)
         if form.is_valid():
